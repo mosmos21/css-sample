be1eebca56cca0f3d6cdbafeb9846579e32e80a5
Takahito Hashimoto <ta.li.ryu.1994@gmail.com>
2019-03-19 22:25:38 +0900

    [add] 項目「内側の角丸」追加

---
+++ public/04/04.js
@@ -0,0 +1,29 @@
+applyCss(
+  '.inner-radius1',
+  {
+    background: '#655',
+    padding: '.8em'
+  },
+  '#css-text1'
+)
+
+applyCss(
+  '.inner-radius1 > div',
+  {
+    background: 'tan',
+    'border-radius': '.8em',
+    padding: '1em'
+  },
+  '#css-text1'
+)
+
+applyCss(
+  '.inner-radius2',
+  {
+    background: 'tan',
+    'border-radius': '.8em',
+    'box-shadow': '0 0 0 .5em #655',
+    outline: '.6em solid #655'
+  },
+  '#css-text2'
+)
